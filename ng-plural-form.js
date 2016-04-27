5ede76831bd9cff9933e0f47da765211fd08761f
Sleepwalker <mail@asleepwalker.ru>
2016-04-28 02:20:18 +0300

    Add lib scratch

---
+++ ng-plural-form.js
@@ -0,0 +1,11 @@
+app.filter('pluralForm', function() {
+	return function(number, forms) {
+		if (number % 10 == 1 && number % 100 != 11) {
+			return forms[0];
+		} else if (number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20)) {
+			return forms[1];
+		} else {
+			return forms[2];
+		}
+	};
+});
