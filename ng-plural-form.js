63e8676729efb1fda1a5d6f6906c634d161b492b
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2016-12-15 14:19:38 +0200

    Fix incorrect form in case of numbers below zero

--- ng-plural-form.js
+++ ng-plural-form.js
@@ -1,5 +1,6 @@
 angular.module('pluralForm', []).filter('pluralForm', function() {
-	return function(number, forms) {]
+	return function(number, forms) {
+		number = Math.abs(number);
 		if (number % 10 == 1 && number % 100 != 11) {
 			return forms[0];
 		} else if (number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20)) {
